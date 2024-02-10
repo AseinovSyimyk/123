cbf95bbb9a52a5f5b562227ac66f0baea78c9ae2
AseinovSyimyk <aseinovsyimyk52@gmail.com>
2024-02-10 14:31:45 +0600

    Initial Commit

---
+++ 123/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  123
+//
+//  Created by User on 10/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
